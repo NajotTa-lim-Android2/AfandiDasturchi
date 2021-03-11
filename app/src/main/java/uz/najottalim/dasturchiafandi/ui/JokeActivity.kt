d0a5db12c4a9895eddd4b3d7e8fe57f94dacb7da
Abdulaziz Abduqodirov <abdulazizabduqodirov13@gmail.com>
2021-03-11 16:09:45 +0500

    Viewmodel added

---
+++ app/src/main/java/uz/najottalim/dasturchiafandi/ui/JokeActivity.kt
@@ -0,0 +1,32 @@
+package uz.najottalim.dasturchiafandi.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import uz.najottalim.dasturchiafandi.databinding.ActivityMainBinding
+import uz.najottalim.dasturchiafandi.viewmodel.JokeViewModel
+
+class JokeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewModel: JokeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(JokeViewModel::class.java)
+
+        viewModel.jokeLive.observe(this, Observer { jokeLive ->
+
+            binding.jokeSetupTxt.text = jokeLive.setup
+            binding.jokePunchlineTxt.text = jokeLive.punchline
+
+        })
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/uz/najottalim/dasturchiafandi/viewmodel/JokeViewModel.kt
@@ -0,0 +1,51 @@
+package uz.najottalim.dasturchiafandi.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import uz.najottalim.dasturchiafandi.model.Joke
+import uz.najottalim.dasturchiafandi.network.JokeApi
+
+class JokeViewModel : ViewModel() {
+
+    val jokeLive = MutableLiveData<Joke>()
+
+    init {
+
+        JokeApi.jokeService.getRandomJoke().enqueue(object : Callback<Joke> {
+
+            override fun onResponse(call: Call<Joke>, response: Response<Joke>) {
+
+                if (response.isSuccessful) {
+
+                    val body = response.body()
+
+
+                    if (body != null) {
+                        val joke: Joke = body
+
+                        jokeLive.value = joke
+
+                    } else {
+
+
+//                        Snackbar.make(binding.root, "Ma'lumotni olishda xatolik yuz berdi", Snackbar.LENGTH_SHORT).show()
+                    }
+
+                } else {
+//                    Snackbar.make(binding.root, "Ma'lumotni olishda xatolik yuz berdi", Snackbar.LENGTH_SHORT).show()
+                }
+
+            }
+
+            override fun onFailure(call: Call<Joke>, t: Throwable) {
+
+//                Log.d(TAG, "onFailure: sldkf")
+//                Snackbar.make(binding.root, "Requestni jo'natishda xatolik", Snackbar.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+}
